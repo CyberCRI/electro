4279410cca66bf8e528235c40e829a9304976083
samonaisi <sam.onaisi@cri-paris.org>
2025-03-07 11:46:22 +0100

    ✨ Use `electro`with websockets (#5)
    * remove discord tweeks
    
    * fix typing
    
    * add vscode settings
    
    * run with docker
    
    * actually return message
    
    * use test_flow instead of main.py file
    
    * create websocket interface
    
    * run make style
    
    * handle multiple users
    
    * separate api and websocket apps

--- electro/app.py
+++ electro/app.py
@@ -1,10 +1,12 @@
 """The API server that works as an endpoint for all the Electro Interfaces."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.websockets import WebSocketState
 from tortoise.contrib.fastapi import register_tortoise
 
 from . import types_ as types
 from .flow_manager import global_flow_manager
+from .interfaces import APIInterface, WebSocketInterface
 from .toolkit.tortoise_orm import get_tortoise_config
 
 app = FastAPI(
@@ -15,18 +17,32 @@ app = FastAPI(
     # redoc_url=None,
 )
 
+api_app = app
+websocket_app = app
 
-@app.post("/message")
-async def process_message(message: types.Message) -> list[types.MessageToSend] | None:
+
+@api_app.post("/message")
+async def process_message(message: types.Message) -> types.MessageToSend | None:
     """Process the message."""
+    manager = APIInterface()
+    return await global_flow_manager.on_message(message, manager)
 
-    return await global_flow_manager.on_message(message)
+
+@websocket_app.websocket("/websocket/client/{client_name}/user/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, client_name: str, user_id: str):
+    manager = WebSocketInterface()
+    await manager.connect(websocket)
+    try:
+        while websocket.application_state == WebSocketState.CONNECTED:
+            data = await websocket.receive_json()
+            data = types.Message.model_validate(data)
+            await global_flow_manager.on_message(data, manager)
+    except WebSocketDisconnect:
+        await manager.disconnect()
 
 
 # region Register Tortoise
-register_tortoise(
-    app,
-    config=get_tortoise_config(),
-)
+register_tortoise(api_app, config=get_tortoise_config())
+register_tortoise(websocket_app, config=get_tortoise_config())
 
 # endregion
--- examples/test_flow.py
+++ examples/test_flow.py
@@ -2,11 +2,9 @@
 
 import uvicorn
 
-from electro.app import app
+from electro.app import websocket_app
 from electro import Flow, MessageFlowStep
 from electro.extra.i18n_gettext import templated_gettext as _
-
-from electro.toolkit.discord_tweeks import enable_message_separator
 
 from electro.triggers import CommandTrigger
 
@@ -17,106 +15,15 @@ class TestFlow(Flow):
     """Test Flow."""
 
     _triggers = [
-        CommandTrigger("test_flow"),
+        CommandTrigger("test"),
     ]
 
-    send_test_message = MessageFlowStep(
-        _("test_flow_message"),
-    )
+    send_test_message_1 = MessageFlowStep(_("test_flow_message"))
+    send_test_message_2 = MessageFlowStep(_("test_flow_message_2"))
 
 
 global_flow_manager.add_flow(TestFlow())
 
-# region Setting up the bot
-# TODO: [2024-07-20 by Mykola] Move to a separate file
-enable_message_separator()
-#
-#
-# @bot.event
-# async def on_connect():
-#     """Start the services when the bot is ready."""
-#     logger.info(f"Logged in as {bot.user.name} ({bot.user.id})")
-#
-#     logger.info(f"Starting the Tortoise ORM...")
-#     await init_orm()
-#
-#     # Save the bot to the database
-#     if not (bot_user := await User.get_or_none(id=bot.user.id)):
-#         bot_user = await User.create(
-#             id=bot.user.id,
-#             username=bot.user.name,
-#             discriminator=bot.user.discriminator,
-#             is_bot=True,
-#         )
-#
-#         logger.info(f"Saved the bot to the database: {bot_user=}")
-#     else:
-#         logger.debug(f"The bot is already in the database: {bot_user=}")
-#
-#
-# @bot.listen("on_member_join")
-# async def on_member_join(member: discord.Member):
-#     await flow_manager.on_member_join(member)
-#
-#
-# @bot.listen("on_member_update")
-# async def on_member_update(before: discord.Member, after: discord.Member):
-#     await flow_manager.on_member_update(before, after)
-#
-#
-# @bot.listen("on_interaction")
-# async def on_interaction(interaction: discord.Interaction):
-#     await flow_manager.on_interaction(interaction)
-#
-#
-# @bot.event
-# async def on_message(message: discord.Message):
-#     """Handle messages."""
-#     return await flow_manager.on_message(message)
-#
-#
-# # On bot joining a Guild (server), add that Guild to the database
-# @bot.event
-# async def on_guild_available(guild: discord.Guild):
-#     """Handle the bot joining a Guild (server)."""
-#     # Save the Guild to the database
-#     guild_, is_created = await Guild.get_or_create(
-#         id=guild.id,
-#         defaults=dict(
-#             name=guild.name,
-#             icon=guild.icon.url if guild.icon else None,
-#             banner=guild.banner.url if guild.banner else None,
-#             description=guild.description,
-#             preferred_locale=guild.preferred_locale,
-#             afk_channel_id=guild.afk_channel.id if guild.afk_channel else None,
-#             afk_timeout=guild.afk_timeout,
-#             owner_id=guild.owner.id,
-#         ),
-#     )
-#
-#     if is_created:
-#         logger.info(f"Created a new Guild: {guild_=}")
-#     else:
-#         logger.info(f"Found an existing Guild: {guild_=}")
-#
-#
-# # Display a warning message on message edit
-# @bot.event
-# async def on_message_edit(before: discord.Message, after: discord.Message):
-#     """Handle message edits."""
-#     if before.author == bot.user:
-#         return
-#
-#     if before.content != after.content:
-#         await after.channel.send(
-#             _("message_edit_warning").safe_substitute(
-#                 user_mention=before.author.mention,
-#             ),
-#             delete_after=5,
-#         )
-
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, loop="asyncio")
-
-# endregion
+    uvicorn.run(app=websocket_app, loop="asyncio", port=8000, host="0.0.0.0")
